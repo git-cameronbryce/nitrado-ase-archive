95f8a2b6f3853d338a54b424feafd87080d7d1ce
git-cameronbryce <developmentcameronbryce@gmail.com>
2026-04-22 07:35:26 -0600

    feature: add axios client and bottleneck

--- source/script.ts
+++ source/script.ts
@@ -2,13 +2,31 @@ import { db } from "./database/client";
 import { eq } from "drizzle-orm";
 import axios from "axios";
 
-import { accountsTable, playersTable, serversTable } from "./database/schema";
+import Bottleneck from "bottleneck";
+import { accountsTable, serversTable } from "./database/schema";
+import { getPlayers } from "./services/players/getPlayers";
 import { getServers } from "./services/servers/getServers";
+import { getLogs } from "./services/logs/getLogs";
 
 const main = async (): Promise<void> => {
   const accounts = await db.select().from(accountsTable);
 
   for (const account of accounts) {
+    const limiter = new Bottleneck({
+      maxConcurrent: 3,
+      minTime: 333,
+    });
+
+    const client = axios.create({
+      baseURL: "https://api.nitrado.net",
+      headers: { Authorization: `Bearer ${account.token}` },
+    });
+
+    // prettier-ignore
+    client.delete = limiter.wrap(client.delete.bind(client)) as typeof client.delete;
+    client.post = limiter.wrap(client.post.bind(client)) as typeof client.post;
+    client.get = limiter.wrap(client.get.bind(client)) as typeof client.get;
+
     const servers = await db
       .select()
       .from(serversTable)
@@ -16,15 +34,19 @@ const main = async (): Promise<void> => {
 
     await Promise.all(
       servers.map(async (server) => {
-        const client = axios.create({
-          baseURL: "https://api.nitrado.net",
-          headers: { Authorization: `Bearer ${account.token}` },
-        });
-
+        await getPlayers(client, server);
         await getServers(client, server);
+        await getLogs(client, server);
       }),
     );
   }
+
+  console.log("Data updated successfully");
 };
 
-main();
+const timer = async () => {
+  await main();
+  setTimeout(timer, 60000);
+};
+
+timer();
